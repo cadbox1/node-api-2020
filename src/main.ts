26bdf3dc5850579c8dd58cca3c4bfeecbde89ba8
cadellchristo <c.christo@damstratechnology.com>
2020-04-10 08:54:31 +0800

    updating the project style
    update the readme
    remove editorconfig
    remove eslint
    add nvmrc
    add my prettier preferences
    remove travis.yml
    remove license
    remove funding

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,9 @@
  * Some predefined delays (in milliseconds).
  */
 export enum Delays {
-  Short = 500,
-  Medium = 2000,
-  Long = 5000,
+	Short = 500,
+	Medium = 2000,
+	Long = 5000,
 }
 
 /**
@@ -15,12 +15,12 @@ export enum Delays {
  * @returns {Promise<string>}
  */
 function delayedHello(
-  name: string,
-  delay: number = Delays.Medium,
+	name: string,
+	delay: number = Delays.Medium
 ): Promise<string> {
-  return new Promise((resolve: (value?: string) => void) =>
-    setTimeout(() => resolve(`Hello, ${name}`), delay),
-  );
+	return new Promise((resolve: (value?: string) => void) =>
+		setTimeout(() => resolve(`Hello, ${name}`), delay)
+	);
 }
 
 // Below are examples of using ESLint errors suppression
@@ -28,5 +28,5 @@ function delayedHello(
 
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 export async function greeter(name: string) {
-  return await delayedHello(name, Delays.Long);
+	return await delayedHello(name, Delays.Long);
 }
